76812c8236964db250c760c7ee18753e19ec7660
Yooondooo <kozyakovdanil04@gmail.com>
2022-10-04 14:32:43 +0300

    new version

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -202,8 +202,8 @@ fun firstDuplicateIndex(str: String): Int {
     val tr = str.lowercase()
     val res = tr.split(" ")
     var sum = 0
-    if (res.size < 2 || (res.size == 2 && res[0] != res[1])) return -1
-    for (i in 0..res.size - 1) {
+    if (res.size < 2) return -1
+    for (i in 0..res.size - 2) {
         if (res[i] == res[i + 1]) {
             return sum
         }
