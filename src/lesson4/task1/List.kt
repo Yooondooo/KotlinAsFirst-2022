4c7ac7f23ee3c156742625fb3aacee4680d43169
Yooondooo <kozyakovdanil04@gmail.com>
2022-10-08 12:21:17 +0300

    new version

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -136,17 +136,8 @@ fun abs(v: List<Double>): Double {
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
 fun mean(list: List<Double>): Double {
-    var mid = 0.0
-    var count = 0
-    for (i in 0..list.size - 1) {
-        mid += list[i]
-        count += 1
-    }
-    mid = mid / count
-    return when {
-        list.isEmpty() -> 0.0
-        else -> mid
-    }
+    if (list.size == 0) return 0.0
+    else return list.sum() / list.size
 }
 
 /**
@@ -160,7 +151,7 @@ fun mean(list: List<Double>): Double {
 fun center(list: MutableList<Double>): MutableList<Double> {
     val md = mean(list)
     for (i in 0..list.size - 1) {
-        list[i] = list[i] - md
+        list[i] -= md
     }
     return list
 }
@@ -177,10 +168,7 @@ fun times(a: List<Int>, b: List<Int>): Int {
     for (i in 0..a.size - 1) {
         c += a[i] * b[i]
     }
-    return when {
-        a.isEmpty() -> 0
-        else -> c
-    }
+    return c
 }
 
 /**
@@ -196,7 +184,7 @@ fun polynom(p: List<Int>, x: Int): Int {
     var count = 0
     val x1 = x.toDouble()
     for (i in 0..p.size - 1) {
-        mnch += (p[i] * (x1.pow(i.toDouble()).toInt()))
+        mnch += (p[i] * (x1.pow(i).toInt()))
     }
     return when {
         p.isEmpty() -> 0
@@ -254,8 +242,8 @@ fun factorizeToString(n: Int): String = TODO()
 fun convert(n: Int, base: Int): List<Int> {
     val res = mutableListOf<Int>()
     var n1 = n
-    when{
-        n1 ==0 -> res.add(0)
+    when {
+        n1 == 0 -> res.add(0)
     }
     while (n1 > 0) {
         res.add(n1 % base)
@@ -279,20 +267,18 @@ fun convertToString(n: Int, base: Int): String {
     var st = ""
     var n1 = n
     var c = 0
-    val alf = listOf<Char>(
-        'a', 'b', 'c', 'd', 'e', 'f',
-        'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x',
-        'y', 'z'
-    )
+    var sti = listOf<Int>()
+    val alf = "abcdefghijklmnopqrstuvwxyz"
     when {
-        n1 == 0 -> st+="0"
+        n1 == 0 -> st += "0"
     }
     when {
-        base <= 10 -> while (n1 > 0) {
-            st = (n1 % base).toString() + st
-            n1 /= base
+        base <= 10 -> {
+            sti = convert(n, base)
+            for (i in sti) {
+                st += i.toString()
+                st.reversed()
+            }
         }
 
         else -> while (n1 > 0) {
@@ -338,31 +324,18 @@ fun decimal(digits: List<Int>, base: Int): Int {
  * (например, str.toInt(base)), запрещается.
  */
 fun decimalFromString(str: String, base: Int): Int {
-    var sum = 0
-    val b = base.toDouble()
-    var st = 0
-    val alf = listOf<Char>(
-        '0', '1', '2', '3', '4',
-        '5', '6', '7', '8', '9',
-        'a', 'b', 'c', 'd', 'e',
-        'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o',
-        'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y',
-        'z'
-    )
-    for (i in str.length - 1 downTo 0) {
-        for (j in 0..alf.size - 1) {
-            when {
-                str[i] == alf[j] -> {
-                    sum += j * (b.pow(st).toInt())
-                    st += 1
-                    break
-                }
+    val st = str
+    val lip = mutableListOf<Int>()
+    val alf = "0123456789abcdefghijklmnopqrstuvwxyz"
+    for (i in st) {
+        for (j in 0..alf.length - 1) {
+            if (i == alf[j]) {
+                lip += j
+                break
             }
         }
     }
-    return sum
+    return decimal(lip, base)
 }
 
 /**
@@ -376,72 +349,16 @@ fun decimalFromString(str: String, base: Int): Int {
 fun roman(n: Int): String {
     var n1 = n
     var st = ""
-    while (n1 > 0) {
-        when {
-            n1 >= 1000 -> {
-                st += 'M'
-                n1 -= 1000
-            }
-
-            n1 >= 900 -> {
-                st += "CM"
-                n1 -= 900
-            }
-
-            n1 >= 500 -> {
-                st += 'D'
-                n1 -= 500
-            }
-
-            n1 >= 400 -> {
-                st += "CD"
-                n1 -= 400
-            }
-
-            n1 >= 100 -> {
-                st += 'C'
-                n1 -= 100
-            }
-
-            n1 >= 90 -> {
-                st += "XC"
-                n1 -= 90
-            }
-
-            n1 >= 50 -> {
-                st += 'L'
-                n1 -= 50
-            }
-
-            n1 >= 40 -> {
-                st += "XL"
-                n1 -= 40
-            }
-
-            n1 >= 10 -> {
-                st += 'X'
-                n1 -= 10
-            }
-
-            n1 >= 9 -> {
-                st += "IX"
-                n1 -= 9
-            }
-
-            n1 >= 5 -> {
-                st += 'V'
-                n1 -= 5
-            }
-
-            n1 >= 4 -> {
-                st += "IV"
-                n1 -= 4
-            }
-
-            n1 >= 1 -> {
-                st += 'I'
-                n1 -= 1
-            }
+    val r = mapOf(
+        1000 to 'M', 900 to "CM", 500 to 'D',
+        400 to "CD", 100 to 'C', 90 to "XC", 50 to 'L',
+        40 to "XL", 10 to 'X', 9 to "IX", 5 to 'V', 4 to "IV",
+        1 to 'I'
+    )
+    for (i in r) {
+        while (n1 >= i.key) {
+            st += i.value
+            n1 -= i.key
         }
     }
     return st
@@ -455,104 +372,59 @@ fun roman(n: Int): String {
  * 23964 = "двадцать три тысячи девятьсот шестьдесят четыре"
  */
 fun russian(n: Int): String {
-    var fir = 0
-    var sec = 0
+    var thou = 0
+    var unit = 0
     var st = ""
     when {
         n >= 1000 -> {
-            fir = n / 1000
-            sec = n % 1000
+            thou = n / 1000
+            unit = n % 1000
         }
 
-        else -> sec = n
+        else -> unit = n
     }
-    val fir1 = fir / 100
-    val fir2 = fir / 10 % 10
-    val fir3 = fir % 10
-    val sec1 = sec / 100
-    val sec2 = sec / 10 % 10
-    val sec3 = sec % 10
-    when {
-        n >= 1000 && sec1 + sec2 + sec3 > 0 -> st =
-            numinStr(fir1, fir2, fir3) + last(fir3, 1, fir2) +
-                    thousend(fir2, fir3) + " " +
-                    numinStr(sec1, sec2, sec3) +
-                    last(sec3, 0, sec2)
-
-        n >= 1000 && sec1 + sec2 + sec3 == 0 -> st =
-            numinStr(fir1, fir2, fir3) + last(fir3, 1, fir2) +
-                    thousend(fir2, fir3)
-
-        else -> st = numinStr(sec1, sec2, sec3) + last(sec3, 0, sec2)
-    }
+    val thou1 = thou / 100
+    val thou2 = thou / 10 % 10
+    val thou3 = thou % 10
+    val unit1 = unit / 100
+    val unit2 = unit / 10 % 10
+    val unit3 = unit % 10
+    if (n >= 1000) st = numinStr(thou1, thou2, thou3) + last(thou3, 1, thou2) +
+            thousend(thou2, thou3) + numinStr(unit1, unit2, unit3) + last(unit3, 0, unit2)
+    else st = numinStr(unit1, unit2, unit3) + last(unit3, 0, unit2)
     return st.trim()
 }
 
 fun numinStr(fir1: Int, fir2: Int, fir3: Int): String {
     var st = ""
-    when {
-        fir1 == 1 -> st += "сто "
-        fir1 == 2 -> st += "двести "
-        fir1 == 3 -> st += "триста "
-        fir1 == 4 -> st += "четыреста "
-        fir1 == 5 -> st += "пятьсот "
-        fir1 == 6 -> st += "шестьсот "
-        fir1 == 7 -> st += "семьсот "
-        fir1 == 8 -> st += "восемьсот "
-        fir1 == 9 -> st += "девятьсот "
-    }
-    when {
-        fir2 == 1 && fir3 == 0 ->
-            st += "десять "
-
-        fir2 == 1 && fir3 == 1 ->
-            st += "одиннадцать "
-
-        fir2 == 1 && fir3 == 2 ->
-            st += "двенадцать "
-
-        fir2 == 1 && fir3 == 3 ->
-            st += "тринадцать "
-
-        fir2 == 1 && fir3 == 4 ->
-            st += "четырнадцать "
-
-        fir2 == 1 && fir3 == 5 ->
-            st += "пятнадцать "
-
-        fir2 == 1 && fir3 == 6 ->
-            st += "шестнадцать "
-
-        fir2 == 1 && fir3 == 7 ->
-            st += "семнадцать "
-
-        fir2 == 1 && fir3 == 8 ->
-            st += "восемнадцать "
-
-        fir2 == 1 && fir3 == 9 ->
-            st += "девятнадцать "
-
-        else -> when {
-            fir2 == 2 -> st += "двадцать "
-            fir2 == 3 -> st += "тридцать "
-            fir2 == 4 -> st += "сорок "
-            fir2 == 5 -> st += "пятьдесят "
-            fir2 == 6 -> st += "шестьдесят "
-            fir2 == 7 -> st += "семьдесят "
-            fir2 == 8 -> st += "восемьдесят "
-            fir2 == 9 -> st += "девяносто "
-        }
-    }
+    val hundreds = mapOf<Int, String>(
+        1 to "сто ", 2 to "двести ", 3 to "триста ", 4 to "четыреста ",
+        5 to "пятьсот ", 6 to "шестьсот ", 7 to "семьсот ", 8 to "восемьсот ",
+        9 to "девятьсот "
+    )
+    val exceptions = mapOf<Int, String>(
+        0 to "десять ", 1 to "одиннадцать ", 2 to "двенадцать ",
+        3 to "тринадцать ", 4 to "четырнадцать ", 5 to "пятнадцать ",
+        6 to "шестнадцать ", 7 to "семнадцать ", 8 to "восемнадцать ",
+        9 to "девятнадцать "
+    )
+    val dozens = mapOf<Int, String>(
+        2 to "двадцать ", 3 to "тридцать ", 4 to "сорок ", 5 to "пятьдесят ",
+        6 to "шестьдесят ", 7 to "семьдесят ", 8 to "восемьдесят ", 9 to "девяносто "
+    )
+    if (fir1 > 0) st += hundreds[fir1]
+    if (fir2 == 1) st += exceptions[fir3]
+    else if (fir2 != 0) st += dozens[fir2]
     return st
 }
 
 fun thousend(a: Int, b: Int): String {
     var st = ""
     when {
-        a == 1 || b in 5..9 || b == 0 -> st = "тысяч"
+        a == 1 || b in 5..9 || b == 0 -> st = "тысяч "
         else -> when {
-            b == 1 -> st = "тысяча"
-            else -> st = "тысячи"
+            b == 1 -> st = "тысяча "
+            else -> st = "тысячи "
         }
     }
     return st
@@ -560,35 +432,13 @@ fun thousend(a: Int, b: Int): String {
 
 fun last(fir3: Int, b: Int, a: Int): String {
     var st = ""
-    when {
-        a != 1 ->
-            when {
-                b == 1 -> when {
-                    fir3 == 1 -> st += "одна "
-                    fir3 == 2 -> st += "две "
-                    fir3 == 3 -> st += "три "
-                    fir3 == 4 -> st += "четыре "
-                    fir3 == 5 -> st += "пять "
-                    fir3 == 6 -> st += "шесть "
-                    fir3 == 7 -> st += "семь "
-                    fir3 == 8 -> st += "восемь "
-                    fir3 == 9 -> st += "девять "
-                }
-
-                else -> when {
-                    fir3 == 1 -> st += "один"
-                    fir3 == 2 -> st += "два"
-                    fir3 == 3 -> st += "три"
-                    fir3 == 4 -> st += "четыре"
-                    fir3 == 5 -> st += "пять"
-                    fir3 == 6 -> st += "шесть"
-                    fir3 == 7 -> st += "семь"
-                    fir3 == 8 -> st += "восемь"
-                    fir3 == 9 -> st += "девять"
-                }
-            }
-
-        else -> st = ""
-    }
+    val units = mapOf<Int, String>(
+        1 to "один", 2 to "два", 3 to "три ", 4 to "четыре ",
+        5 to "пять ", 6 to "шесть ", 7 to "семь ", 8 to "восемь ",
+        9 to "девять ", 11 to "одна ", 12 to "две "
+    )
+    if (a != 1)
+        if (fir3 in 3..9) st += units[fir3]
+        else if(fir3 != 0 ) st += units[fir3 + b * 10]
     return st
 }
